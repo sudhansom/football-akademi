7b5c7ae667b726e6c13a3384ab99cf9a5660ee4d
bhuwan <bkspoudel@gmail.com>
2025-01-20 08:56:45 +0100

    delete default and create a navbar component

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+ <h1>Football</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <span>menu</span>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

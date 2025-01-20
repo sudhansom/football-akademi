307cc16167e9c828497631450378dc47acf25ca4
bhuwan <bkspoudel@gmail.com>
2025-01-20 12:37:02 +0100

    loading spinner component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
 import NavBar from "../src/components/NavBar.vue"
+import LoadingSpinner from "./components/LoadingSpinner.vue"
+
 </script>
 
 <template>
- <NavBar />
+ <nav-bar />
+ <div class="main-body">
+  <loading-spinner />
+ </div>
 </template>
 
 <style scoped>
-
+.main-body {
+  min-height: 90dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>

92deea93ab008dc206ed82f65d9f20f137de78b2
bhuwan <bkspoudel@gmail.com>
2025-01-20 09:03:35 +0100

    remove default padding

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu-bar">
+    <div class="bar-line"></div>
+    <div class="bar-line"></div>
+    <div class="bar-line"></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.menu-bar {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    width: 24px;
+    padding: 3px;
+    cursor: pointer;
+}
+.menu-bar:hover .bar-line{
+    background: rgb(198, 196, 196);
+}
+.bar-line {
+    height: 2px;
+    background: black;
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,33 @@
 <template>
-  <nav>
-    <span>menu</span>
+  <nav class="navbar">
+   <section class="navbar-1">
+        <menu-bar />
+        <div class="logo">
+            <img src="../assets/logo.png" width="48px" alt="logo">
+        </div>
+   </section>
   </nav>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import MenuBar from "./MenuBar.vue"
 </script>
 
-<style>
+<style scoped>
+.navbar {
+    border-bottom: 1px solid rgb(213, 210, 210);
+    padding: 6px 12px;
+}
+.navbar-1 {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+}
+.logo {
+    cursor: pointer;
+}
+img {
+    display: block;
+}
 
 </style>

3e92dd32325b9485c61956bf0673e070886105bf
bhuwan <bkspoudel@gmail.com>
2025-01-22 23:19:16 +0100

    fix age height and weight

--- src/views/AllPlayers.vue
+++ src/views/AllPlayers.vue
@@ -2,16 +2,18 @@
     <table>
         <thead>
             <tr>
+                <th></th>
                 <th>Name</th>
                 <th>Address</th>
                 <th>Age</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
+            <tr v-for="i in 10" :key="i">
+                <td>{{i}}</td>
                 <td><router-link to="/info/detail">Sudhan</router-link></td>
-                <td>Lalim</td>
-                <td>40</td>
+                <td>Husum</td>
+                <td>7</td>
             </tr>
         </tbody>
     </table>
--- src/views/PlayersDetail.vue
+++ src/views/PlayersDetail.vue
@@ -1,47 +1,67 @@
 <template>
   <div class="person">
-    <div class="personal-info">
-    <div>
-        <h2>Sudhan Poudel ({{ 7 }})</h2>
-        <p>Started on: 15-11-2024</p>
+    <div class="personal-info card">
+    <div >
+        <h2>Rhythm Som Poudel</h2>
+        <p><b>Started on: 15-11-2024</b></p>
     </div>
     <div class="image">
         <img src="../assets/hrithm.png" alt="">
     </div>
   </div>
-  <table>
-    <h3>Payments:</h3>
-    <thead>
-        <tr>
-            <th>Jan</th>
-            <th>Feb</th>
-            <th>Mar</th>
-            <th>Apr</th>
-            <th>Jun</th>
-            <th>Jul</th>
-            <th>Aug</th>
-            <th>Sep</th>
-            <th>Oct</th>
-            <th>Nov</th>
-            <th>Dec</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th>paid</th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-        </tr>
-    </tbody>
-  </table>
+  <div class="height-weight card">
+        <p style="font-weight:bold;margin:0;"><span>Age: {{ 7.4}} yrs</span> | <span title="Updated on jan-2025">Height: {{ 130}}cm</span> | <span title="Updated on jan-2025">Weight: {{32}}kg</span></p>
+  </div>
+  <div class="payment card">
+    <table>
+        <h3>Payments:</h3>
+        <thead>
+            <tr>
+                <th>Jan</th>
+                <th>Feb</th>
+                <th>Mar</th>
+                <th>Apr</th>
+                <th>Jun</th>
+                <th>Jul</th>
+                <th>Aug</th>
+                <th>Sep</th>
+                <th>Oct</th>
+                <th>Nov</th>
+                <th>Dec</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>paid</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+  <div class="about card">
+    <h3>Skills:</h3>
+    <ol>
+        <li>good running (fast )</li>
+        <li>very interested and motivated</li>
+        <li>he is not good at dribbling</li>
+        <li>not good shoot</li>
+        <li>slow with ball</li>
+        <li>week left leg</li>
+    </ol>
+  </div>
+    <div class="s-training card">
+    <h3>Specific Training</h3>
+    <p>Not taken yet.</p>
+    </div>
   </div>
 </template>
 
@@ -75,6 +95,20 @@ h3 {
 .person {
     border: 1px solid gray;
     padding: 12px;
+    border-radius: 12px;
+    margin: 24px;
+}
+ol {
+    padding:0;
+    margin: 0;
+}
+li {
+    margin-left: 24px;
+}
+.card {
+    background: rgb(236, 235, 235);
+    padding: 12px;
+    margin: 12px 0;
     border-radius: 12px;
 }
 </style>

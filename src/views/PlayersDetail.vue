ff230fe78fc0ca2fc5a9c13268117c2e392502eb
bhuwan <bkspoudel@gmail.com>
2025-01-26 09:02:22 +0100

    make personall info dynamic

--- src/views/PlayersDetail.vue
+++ src/views/PlayersDetail.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="person">
+  <div v-if="user" class="person">
     <div class="flex justify-between card">
     <div >
-        <h2 class="font-bold text-2xl mb-3">Rhythm Som Poudel</h2>
-        <p><b>Started on: 15-11-2024</b></p>
+        <h2 class="font-bold text-2xl mb-3">{{ user.name }}</h2>
+        <p><b>Started on: {{user.joined}}</b></p>
     </div>
     <div class="border-gray-300 h-32 w-32">
         <img src="../assets/hrithm.png" alt="">
     </div>
   </div>
   <div class="height-weight card">
-        <p style="font-weight:bold;margin:0;"><span>Age: {{ 7.4}} yrs</span> | <span title="Updated on jan-2025">Height: {{ 130}}cm</span> | <span title="Updated on jan-2025">Weight: {{32}}kg</span></p>
+        <p style="font-weight:bold;margin:0;"><span>Age: {{ 7.4 }} yrs</span> | <span title="Updated on jan-2025">Height: {{ user.personal.height}}cm</span> | <span title="Updated on jan-2025">Weight: {{user.personal.weight}}kg</span></p>
   </div>
   <div class="payment card">
     <table>
@@ -63,10 +63,24 @@
     <p>Not taken yet.</p>
     </div>
   </div>
+  <div v-else class="person flex justify-center items-center">
+    <loading-spinner></loading-spinner>
+  </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import LoadingSpinner from "../components/LoadingSpinner.vue";
 
+const user = ref(null)
+
+onMounted(()=>{
+    fetch('http://localhost:3002/users/1')
+    .then(response => response.json())
+    .then(data => {
+        user.value = data
+    })
+})
 </script>
 
 <style scoped>

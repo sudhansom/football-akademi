498c930897070683b6d78fb6929fbbb0a2cc7b90
bhuwan <bkspoudel@gmail.com>
2025-02-05 12:33:02 +0100

    add actions and getters(computed)

--- stores/CounterStore.js
+++ stores/CounterStore.js
@@ -1,9 +1,25 @@
 import { defineStore } from "pinia";
 
 export let useCounterStore = defineStore('counter', {
+    // data
     state(){
         return {
             count:0
         };
+    }, 
+    //actions
+    actions: {
+        increament(){
+            this.count ++;
+        }, 
+        decreament(){
+            this.count--;
+        }
+    },
+    // compouted
+    getters: {
+        remaining(){
+            return 100 - this.count;
+        }
     }
 });

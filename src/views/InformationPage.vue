e38de796fa0227150218bc295cfb0555580c1f6b
bhuwan <bkspoudel@gmail.com>
2025-02-08 21:36:41 +0100

    show all users only for loggedin users

--- src/views/InformationPage.vue
+++ src/views/InformationPage.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="info">
     <div class="links">
-        <router-link class="each-tab" to="/info">General</router-link>
-        <router-link class="each-tab" to="/info/all-players" >Players</router-link>
-        <router-link class="each-tab" to="/info/important-message" >Message</router-link>
+        <router-link v-if="role" class="each-tab" to="/info">General</router-link>
+        <router-link v-if="role" class="each-tab" to="/info/all-players" >Players</router-link>
+        <router-link v-if="role==='admin'" class="each-tab" to="/info/important-message" >Message</router-link>
     </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+const role = ref(localStorage.getItem("userRole"))
 
 </script>
 

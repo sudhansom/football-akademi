bc0eb969f67fbef78114f47ada652c714784d69c
bhuwan <bkspoudel@gmail.com>
2025-02-15 13:11:48 +0100

    update localstorage value to store while app starts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 import NavBar from "../src/components/NavBar.vue"
 import TheFooter from "./components/TheFooter.vue"
+import { useUserStore } from "./stores/UserStore"
+import { useFetchData } from "./composables/useFetchData"
 
+const { data, error, loading, load } = useFetchData()
+const users = useUserStore();
+
+const userId = ref(localStorage.getItem('userId'))
+const token = ref(localStorage.getItem("token"));
+if(userId.value){
+  setUser();
+}
+async function setUser(){
+  await load('/users/'+ userId.value)
+  users.fillCurrentUser(data.value);
+  users.setToken(token.value);
+}
 
 </script>
 

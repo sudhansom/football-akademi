9c4c95cb3b5d544f9cfd56122c7eff6b2496da31
bhuwan <bkspoudel@gmail.com>
2025-02-27 22:24:41 +0100

    add phone number

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center  md:flex-row md:justify-between p-2 bg-gray-300 ">
-     <span>Contact: 0045-123456</span>
+     <span>Contact: 0045-81910081</span>
      <span>@copyright 2025 | All Rights Reserved</span>
   </div>
 </template>

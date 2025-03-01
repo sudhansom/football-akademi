b5504db0ace23e26dc6367993f8f4b31d506ca0f
bhuwan <bkspoudel@gmail.com>
2025-03-01 20:54:44 +0100

    change the bg color of footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center  md:flex-row md:justify-between p-2 bg-gray-300 ">
+  <div class="flex flex-col justify-center items-center  md:flex-row md:justify-between p-2 text-white bg-stone-400 ">
      <span>Contact: 0045-81910081</span>
      <span>@copyright 2025 | All Rights Reserved</span>
   </div>

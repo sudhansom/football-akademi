ad57e0ff1bc79cdf6d00fd4b8b3a223b15cf5baa
bhuwan <bkspoudel@gmail.com>
2025-03-02 23:09:01 +0100

    style my about me section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,11 +1,19 @@
 <template>
-  <h4>About Me:</h4>
+  <div class="container">
+    <h4 class="text-center font-bold">About Me:</h4>
+  </div>
 </template>
 
 <script setup>
 
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  padding: 12px;
+  border: 1px solid gray;
+  margin: 24px auto;
+  border-radius: 12px;
+  max-width: 500px;
+}
 </style>

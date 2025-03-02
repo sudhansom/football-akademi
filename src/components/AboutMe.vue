4474c1fb5b71879001f7c64dbb1777d0055e4dab
bhuwan <bkspoudel@gmail.com>
2025-03-02 23:15:16 +0100

    fix title and description

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
-    <h4 class="text-center font-bold">About Me:</h4>
+    <h4 class="text-center font-bold">{{ title }}</h4>
+    <p>{{ description }}</p>
   </div>
 </template>
 
 <script setup>
-
+const title = "Ayoub Sørensen Academy – Where Champions Are Made!"
+const description = "Discover the Ayoub Sørensen Academy, where passion meets excellence in football! Founded by national futsal star Ayoub Sørensen, our academy is dedicated to nurturing talent, building skills, and fostering a love for the game. Whether you're a beginner or an aspiring pro, our expert coaches provide personalized training, cutting-edge techniques, and a supportive environment to help you reach your full potential. Join a community of driven players, train in top-tier facilities, and learn from one of the best in the sport. Elevate your game with the Ayoub Sørensen Academy – where champions are made! Enroll today and take the first step toward futsal greatness!"
 </script>
 
 <style scoped>
